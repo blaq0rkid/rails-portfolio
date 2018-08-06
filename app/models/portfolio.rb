8fb5a6d5ecabffbdb46eb80bf243f6168f590286
blaq0rkid <kurozato.candy@gmail.com>
2018-08-06 05:36:38 +0000

    implemented form configuration for nested attributes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,7 +6,7 @@ class Portfolio < ApplicationRecord
     include Placeholder
     validates_presence_of :title, :body, :main_image, :thumb_image
 
-    after_initilize :set_defaults
+    after_initialize :set_defaults
     
     def set_defaults
         self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
