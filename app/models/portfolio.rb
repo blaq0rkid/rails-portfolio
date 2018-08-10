5d9c4ea38597445fe51d3808e89a1ce91541c3e1
blaq0rkid <kurozato.candy@gmail.com>
2018-08-10 04:01:09 +0000

    updated portfolio to include attribute for position

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,10 +1,16 @@
 class Portfolio < ApplicationRecord
     has_many :technologies
     accepts_nested_attributes_for :technologies, 
-    reject_if: lambda {|attrs| attrs ['name']}
+                                reject_if: lambda {|attrs| attrs ['name']}
     
     include Placeholder
     validates_presence_of :title, :body, :main_image, :thumb_image
+    
+    def self.by_position
+        order("position ASC")
+    end
+    
+    scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Ruby on Rails') }
 
     after_initialize :set_defaults
     
