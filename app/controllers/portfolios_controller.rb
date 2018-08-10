33c9e1c79d684e2b2ad8426c786bee857cc9c907
blaq0rkid <kurozato.candy@gmail.com>
2018-08-10 10:48:53 +0000

    implemenmted initital file uploading system via carrierwave for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,65 +1,76 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:update, :edit, :show, :destroy]
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
   
-    def index
-        @portfolio_items = Portfolio.all
-    end
-    
-    def new
-        @portfolio_item = Portfolio.new
-        3.times {@portfolio_item.technologies.build}
-    end
-    
-    def create
-        @portfolio_item = Portfolio.new(portfolio_params)
-        
-        respond_to do |format|
-          if @portfolio_item.save
-            format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
-          else
-            format.html { render :new }
-          end
-        end
-    end
-    
-    def edit
-    end
-    
-    def update
+  def index
+    @portfolio_items = Portfolio.by_position
+  end
 
-        respond_to do |format|
-          if @portfolio_item.update(portfolio_params)
-            format.html { redirect_to portfolios_path, notice: 'The record was successfully created.' }
-          else
-            format.html { render :edit }
-          end
-        end
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
     end
-    
-    def show
+
+    render nothing: true
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
     end
-    
-    def destroy
-        @portfolio_item.destroy
-        respond_to do |format|
-          format.html { redirect_to portfolios_url, notice: 'The record was successfully deleted.' }
-        end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
     end
-    
-    private 
-    
-    def portfolio_params
-      params.require(:portfolio).permit(:title, 
-                                :subtitle, 
-                                :body, 
-                                technologies_attributes: [:name]
-                                )
+  end
+
+  def show
+  end
+
+  def destroy
+    # Destroy/delete the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
-    
-    def set_portfolio_item
-      @portfolio_item = Portfolio.find(params[:id])
-    end
-    
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      :main_image,
+                                      :thumb_image,
+                                      technologies_attributes: [:name]
+                                     )
+  end
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 end
