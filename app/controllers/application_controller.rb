aa816e692c04d31a8e12e0debeeb01e343fc8aa2
blaq0rkid <kurozato.candy@gmail.com>
2018-08-12 05:15:04 +0000

    stil messing around

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  include DeviseWhiteList
+  # include DeviseWhiteList
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
